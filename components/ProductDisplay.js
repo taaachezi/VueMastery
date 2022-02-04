d777ca04921e87a767e77ce50dafa7cc08afbaec
taaachezi <64632134+taaachezi@users.noreply.github.com>
2022-02-04 23:10:31 +0900

    end

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -12,9 +12,7 @@ app.component('product-display', {
         <p v-if="inStock">In Stock</p>
         <p v-else>Out of Stock</p>
         <p>Shipping: {{ shipping }}</p>
-        <ul>
-          <li v-for="detail in details">{{ detail }}</li>
-        </ul>
+        <product-details></product-details>
 
         <div 
           v-for="(variant, index) in variants" 
@@ -33,7 +31,6 @@ app.component('product-display', {
             product: 'Socks',
             brand: 'Vue Mastery',
             selectedVariant: 0,
-            details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
             { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
             { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
